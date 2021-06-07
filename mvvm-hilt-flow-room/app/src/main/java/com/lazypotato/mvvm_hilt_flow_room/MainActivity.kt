0cfd09bff0df0d9f5850ed57f7f40109878b0fd2
Ahmed Fahim Foysal Eishon <ahmed-fahim-foysal-eishon@ib-system.co.jp>
2021-06-07 11:01:15 +0900

    init mvvm-hilt-flow-room project

---
+++ mvvm-hilt-flow-room/app/src/main/java/com/lazypotato/mvvm_hilt_flow_room/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lazypotato.mvvm_hilt_flow_room
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
