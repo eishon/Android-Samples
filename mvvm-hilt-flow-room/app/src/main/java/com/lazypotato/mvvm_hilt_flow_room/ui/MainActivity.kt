2447792b73840711aeb6bd2400173b528b6fc125
Ahmed Fahim Foysal Eishon <ahmed-fahim-foysal-eishon@ib-system.co.jp>
2021-06-07 14:20:27 +0900

    mvvm-hilt-flow-room: Updated Navigation

--- mvvm-hilt-flow-room/app/src/main/java/com/lazypotato/mvvm_hilt_flow_room/ui/MainActivity.kt
+++ mvvm-hilt-flow-room/app/src/main/java/com/lazypotato/mvvm_hilt_flow_room/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.lazypotato.mvvm_hilt_flow_room
+package com.lazypotato.mvvm_hilt_flow_room.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lazypotato.mvvm_hilt_flow_room.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ mvvm-hilt-flow-room/app/src/main/java/com/lazypotato/mvvm_hilt_flow_room/ui/tasks/TasksFragment.kt
@@ -0,0 +1,7 @@
+package com.lazypotato.mvvm_hilt_flow_room.ui.tasks
+
+import androidx.fragment.app.Fragment
+import com.lazypotato.mvvm_hilt_flow_room.R
+
+class TasksFragment: Fragment(R.layout.fragment_tasks) {
+}
