76713a8d0587ef927f05a6f3ea901b3dd29a25c7
Ahmed Fahim Foysal Eishon <ahmed-fahim-foysal-eishon@ib-system.co.jp>
2021-06-14 09:59:26 +0900

    Architecture Component Practice: Handling Process death

--- architechture_components_practice/app/src/main/java/com/lazypotato/architechture_components_practice/ui/gallery/GalleryViewModel.kt
+++ architechture_components_practice/app/src/main/java/com/lazypotato/architechture_components_practice/ui/gallery/GalleryViewModel.kt
@@ -1,9 +1,7 @@
 package com.lazypotato.architechture_components_practice.ui.gallery
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.switchMap
-import androidx.lifecycle.viewModelScope
+import androidx.hilt.Assisted
+import androidx.lifecycle.*
 import androidx.paging.cachedIn
 import com.lazypotato.architechture_components_practice.data.UnsplashRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -11,10 +9,11 @@ import javax.inject.Inject
 
 @HiltViewModel
 class GalleryViewModel @Inject constructor(
-    private val repository: UnsplashRepository
+    private val repository: UnsplashRepository,
+    @Assisted state: SavedStateHandle
 ): ViewModel() {
 
-    private val currentQuery = MutableLiveData(DEFAULT_QUERY)
+    private val currentQuery = state.getLiveData(CURRENT_QUERY, DEFAULT_QUERY)
 
     val photos = currentQuery.switchMap { queryString ->
         repository.getSearchResults(queryString).cachedIn(viewModelScope)
@@ -25,6 +24,7 @@ class GalleryViewModel @Inject constructor(
     }
 
     companion object {
+        private const val CURRENT_QUERY = "current_query"
         private const val DEFAULT_QUERY = "birds"
     }
 }
