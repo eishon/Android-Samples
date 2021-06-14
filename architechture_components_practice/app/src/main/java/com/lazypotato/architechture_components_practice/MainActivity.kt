d1eac215e945c3b09d11720a331c18ca0f37cf54
Ahmed Fahim Foysal Eishon <ahmed-fahim-foysal-eishon@ib-system.co.jp>
2021-06-14 09:48:28 +0900

    Architecture Component Practice: Implementing DetailsFragment

--- architechture_components_practice/app/src/main/java/com/lazypotato/architechture_components_practice/MainActivity.kt
+++ architechture_components_practice/app/src/main/java/com/lazypotato/architechture_components_practice/MainActivity.kt
@@ -2,12 +2,32 @@ package com.lazypotato.architechture_components_practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHOstFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_hos_fragment_main)
+                as NavHostFragment
+        navController = navHOstFragment.findNavController()
+
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
