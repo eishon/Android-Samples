9f58f86c0b5126dee0b3c6a4a04685d8a494319c
Ahmed Fahim Foysal Eishon <ahmed-fahim-foysal-eishon@ib-system.co.jp>
2021-06-21 10:04:35 +0900

    Android Test Practice: Add Shopping Item to DB test

--- AndroidTestPractice/app/src/androidTest/java/com/lazypotato/androidtestpractice/ui/AddShoppingItemFragmentTest.kt
+++ AndroidTestPractice/app/src/androidTest/java/com/lazypotato/androidtestpractice/ui/AddShoppingItemFragmentTest.kt
@@ -1,14 +1,23 @@
 package com.lazypotato.androidtestpractice.ui
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.filters.MediumTest
+import com.google.common.truth.Truth.assertThat
 import com.lazypotato.androidtestpractice.R
+import com.lazypotato.androidtestpractice.data.local.ShoppingItem
+import com.lazypotato.androidtestpractice.getOrAwaitValue
 import com.lazypotato.androidtestpractice.launchFragmentInHiltContainer
+import com.lazypotato.androidtestpractice.repositories.FakeShoppingRepositoryAndroidTest
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -18,6 +27,7 @@ import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito
 import org.mockito.Mockito.verify
+import javax.inject.Inject
 
 @MediumTest
 @HiltAndroidTest
@@ -27,9 +37,33 @@ class AddShoppingItemFragmentTest{
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
 
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Inject
+    lateinit var fragmentFactory: ShoppingFragmentFactory
+
     @Before
     fun setup() {
         hiltRule.inject()
+    }
+
+    @Test
+    fun clickInsertIntoDb_shoppingItemInsertedIntoDb() {
+        val testViewModel = ShoppingViewModel(FakeShoppingRepositoryAndroidTest())
+        launchFragmentInHiltContainer<AddShoppingItemFragment>(
+            fragmentFactory = fragmentFactory
+        ) {
+            viewModel = testViewModel
+        }
+
+        onView(withId(R.id.etShoppingItemName)).perform(replaceText("shopping item"))
+        onView(withId(R.id.etShoppingItemName)).perform(replaceText("5"))
+        onView(withId(R.id.etShoppingItemName)).perform(replaceText("5.5"))
+        onView(withId(R.id.btnAddShoppingItem)).perform(click())
+
+        assertThat(testViewModel.shoppingItems.getOrAwaitValue())
+            .contains(ShoppingItem("shopping item", 5, 5.5f, ""))
     }
 
     @Test
--- AndroidTestPractice/app/src/main/java/com/lazypotato/androidtestpractice/ui/AddShoppingItemFragment.kt
+++ AndroidTestPractice/app/src/main/java/com/lazypotato/androidtestpractice/ui/AddShoppingItemFragment.kt
@@ -4,12 +4,23 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.RequestManager
+import com.google.android.material.snackbar.Snackbar
 import com.lazypotato.androidtestpractice.R
+import com.lazypotato.androidtestpractice.adapters.ImageAdapter
+import com.lazypotato.androidtestpractice.other.Status
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_add_shopping_item.*
 import kotlinx.android.synthetic.main.item_image.*
+import kotlinx.android.synthetic.main.item_image.ivShoppingImage
+import javax.inject.Inject
 
-class AddShoppingItemFragment : Fragment(R.layout.fragment_add_shopping_item){
+class AddShoppingItemFragment @Inject constructor(
+    val glide: RequestManager
+) : Fragment(R.layout.fragment_add_shopping_item){
 
     lateinit var viewModel: ShoppingViewModel
 
@@ -17,6 +28,15 @@ class AddShoppingItemFragment : Fragment(R.layout.fragment_add_shopping_item){
         super.onViewCreated(view, savedInstanceState)
 
         viewModel = ViewModelProvider(requireActivity()).get(ShoppingViewModel::class.java)
+        subscribeToObservers()
+
+        btnAddShoppingItem.setOnClickListener {
+            viewModel.insertShoppingItem(
+                etShoppingItemName.text.toString(),
+                etShoppingItemAmount.text.toString(),
+                etShoppingItemPrice.text.toString()
+            )
+        }
 
         ivShoppingImage.setOnClickListener {
             findNavController().navigate(
@@ -32,5 +52,37 @@ class AddShoppingItemFragment : Fragment(R.layout.fragment_add_shopping_item){
         }
 
         requireActivity().onBackPressedDispatcher.addCallback(callback)
+    }
+
+    private fun subscribeToObservers() {
+        viewModel.curImageUrl.observe(viewLifecycleOwner, Observer {
+            glide.load(it).into(ivShoppingImage)
+        })
+
+        viewModel.insertShoppingItemStatus.observe(viewLifecycleOwner, Observer {
+            it.getContentIfNotHandled()?.let { result ->
+                when(result.status) {
+                    Status.SUCCESS -> {
+                        Snackbar.make(
+                            requireActivity().rootLayout,
+                            "Added shopping item",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                        findNavController().popBackStack()
+                    }
+                    Status.ERROR -> {
+                        Snackbar.make(
+                            requireActivity().rootLayout,
+                            result.message ?: "An unknown error occurred",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                        findNavController().popBackStack()
+                    }
+                    Status.LOADING -> {
+                        /*NO OP*/
+                    }
+                }
+            }
+        })
     }
 }
