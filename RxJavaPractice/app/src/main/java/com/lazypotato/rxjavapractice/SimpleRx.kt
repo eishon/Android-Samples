80e8f4dc87ffda5a8a0a938d90b98fbd8a61fd8c
Ahmed Fahim Foysal Eishon <ahmed-fahim-foysal-eishon@ib-system.co.jp>
2021-07-29 11:51:53 +0900

    RxJava Practice: Basic Observable and Creating Observable

--- RxJavaPractice/app/src/main/java/com/lazypotato/rxjavapractice/SimpleRx.kt
+++ RxJavaPractice/app/src/main/java/com/lazypotato/rxjavapractice/SimpleRx.kt
@@ -1,9 +1,15 @@
 package com.lazypotato.rxjavapractice
 
 import com.jakewharton.rxrelay3.BehaviorRelay
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.subjects.BehaviorSubject
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.lang.IllegalArgumentException
+import java.util.concurrent.TimeUnit
 
 object SimpleRx {
 
@@ -52,5 +58,36 @@ object SimpleRx {
         // 2 onComplete
         behaviorSubject.onComplete()
         behaviorSubject.onNext(110)
+    }
+
+    fun basicObservable() {
+        val observable = Observable.create<String> { observer ->
+            println("~~ Observable Logic being triggered ~~")
+
+            MainScope().launch {
+                delay(1000)
+
+                observer.onNext("some value 23")
+                observer.onComplete()
+
+                delay(1000)
+            }
+        }
+
+        val observer = observable.subscribe { someString ->
+            println("another subscriber: $someString")
+        }
+
+        //observer.dispose()
+    }
+
+    fun creatingObservables() {
+//        val observable = Observable.just(23)
+
+//        val observable = Observable.interval(300, TimeUnit.MILLISECONDS)
+//            .timeInterval(AndroidSchedulers.mainThread())
+
+//        val userIds = arrayOf(1,2,3,4,5,6)
+//        val observable = Observable.fromArray(*userIds)
     }
 }
