ca1a6b39d7d26a763bc3482fcda15bc5b4c57c42
Ahmed Fahim Foysal Eishon <ahmed-fahim-foysal-eishon@ib-system.co.jp>
2021-07-29 11:25:24 +0900

    RxJava Practice: Behavior Subject

--- RxJavaPractice/app/src/main/java/com/lazypotato/rxjavapractice/SimpleRx.kt
+++ RxJavaPractice/app/src/main/java/com/lazypotato/rxjavapractice/SimpleRx.kt
@@ -2,6 +2,8 @@ package com.lazypotato.rxjavapractice
 
 import com.jakewharton.rxrelay3.BehaviorRelay
 import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.subjects.BehaviorSubject
+import java.lang.IllegalArgumentException
 
 object SimpleRx {
 
@@ -24,5 +26,31 @@ object SimpleRx {
         }
 
         someInfo.accept("3")
+    }
+
+    fun simpleBehaviorSubject() {
+        val behaviorSubject = BehaviorSubject.createDefault(24)
+
+        val disposable = behaviorSubject.subscribe ({ newValue ->
+                println("behavior subscription: $newValue")
+            }, { error ->
+                println("error: ${error.localizedMessage}")
+            }, {
+                println("completed")
+            },
+        )
+
+        behaviorSubject.onNext(34)
+        behaviorSubject.onNext(48)
+        behaviorSubject.onNext(48)
+
+        // 1 onError
+//        val someException = IllegalArgumentException("Some Fake Error")
+//        behaviorSubject.onError(someException)
+//        behaviorSubject.onNext(109)
+
+        // 2 onComplete
+        behaviorSubject.onComplete()
+        behaviorSubject.onNext(110)
     }
 }
